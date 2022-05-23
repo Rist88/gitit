c680d2097c48430b41cbf81440cf705a27dfdd6c
Andrew Rudenko <andrew.rudenko9@gmail.com>
2022-05-23 11:40:55 +0300

    add Bill Cipher gif
    Added gif to the view and formatting w/ Prettier

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const res = require('express/lib/response')
-const fs = require('fs')
-const path = require('path')
+const express = require("express");
+const res = require("express/lib/response");
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-    
-    response.sendFile(path.join(__dirname + '/home.html'), (err) => {
-        response.status(500).send(`Error occured: ${err}`)
-    })
-    // fs.readFile('./home.html', 'utf8', (err, html) => {
-    //     if (err) {
-    //     }
-    //     response.send(html)
-    // })
-})
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname + "/home.html"), (err) => {
+    response.status(500).send(`Error occured: ${err}`);
+  });
+  // fs.readFile('./home.html', 'utf8', (err, html) => {
+  //     if (err) {
+  //     }
+  //     response.send(html)
+  // })
+});
 
-app.listen(process.env.PORT || 3000, () => console.log('App avaliable on http://localhost:3000'))
+app.listen(process.env.PORT || 3000, () =>
+  console.log("App avaliable on http://localhost:3000")
+);
